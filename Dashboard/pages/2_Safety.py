e95c8be5ffe64776d1e93fd1da849948abcbd34b
lee-38 <haeinlee38@gmail.com>
2025-07-09 10:35:09 +0900

    Update

--- Dashboard/pages/2_Safety.py
+++ Dashboard/pages/2_Safety.py
@@ -9,14 +9,18 @@ import seaborn as sns
 import google.generativeai as genai
 from pathlib import Path
 from matplotlib import font_manager, rc
+import os
 
 # 나눔고딕 경로 (Streamlit Cloud 기준)
 font_path = "/usr/share/fonts/truetype/nanum/NanumGothic.ttf"
 
-# 폰트 설정
-font_name = font_manager.FontProperties(fname=font_path).get_name()
-rc('font', family=font_name)
-plt.rcParams['axes.unicode_minus'] = False  # 마이너스 깨짐 방지
+# 폰트 존재하면 설정
+if os.path.exists(font_path):
+    font_name = font_manager.FontProperties(fname=font_path).get_name()
+    rc('font', family=font_name)
+    plt.rcParams['axes.unicode_minus'] = False  # 마이너스 깨짐 방지
+else:
+    print("⚠️ 서버에 한글 폰트가 없습니다.")
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 DATA_DIR = BASE_DIR / "Files"
